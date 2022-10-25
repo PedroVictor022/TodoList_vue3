aa11130c3894e218f51d397af01cfea1fdb76a82
Pedro Victor <pedro20002victor@gmail.com>
2022-10-24 21:47:05 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <TodoApp />
+</template>
+
+<script>
+import TodoApp from './component/TodoApp.vue';
+export default {
+  components: {
+    TodoApp,
+  }
+}
+</script>
+
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: sans-serif;
+}
+
+body {
+  background: #222;
+  font-family: sans-serif;
+  padding-bottom: 1em;
+  width: 80%;
+  margin: 0 auto;
+
+}
+</style>
---
+++ src/component/TodoApp.vue
@@ -0,0 +1,79 @@
+<template>
+   <div class="container">
+      <h2 class="title-app">Vue3 Todo App</h2>
+      <form action="">
+         <label>New Todo</label>
+         <input type="text">
+         <button>Add New Todo</button>
+      </form>
+   </div>
+</template>
+
+<script>
+export default {
+   name: "TodoApp",
+
+}
+</script>
+
+<style>
+.container {
+   margin: 0 auto;
+   width: 420px;
+   
+   display: flex;
+   flex-direction: column;
+   align-items:center;
+   justify-content: center;
+}
+.title-app {
+   font-size: 36px;
+   background: -webkit-linear-gradient(#b7f54c, #049674);
+   -webkit-background-clip: text;
+   -webkit-text-fill-color: transparent;
+
+   margin-top: 2rem;
+   margin-bottom: 1rem;
+}
+
+form {
+   display: flex;
+   flex-direction: column;
+   gap:0.8rem;
+
+   padding: 1rem;
+   border-radius: 6px;
+
+   background: #141414;
+
+   width: 320px;
+}
+label {
+   font-size: 22px;
+   color: #fefefe;
+}
+
+input {
+   width: 100%;
+   padding: 0.2rem;
+   border: none;
+   outline: none;
+   border-radius: 4px;
+}
+
+button {
+   cursor: pointer;
+   width: 100%;
+   padding: 0.2rem;
+   border-radius: 4px;
+   background: linear-gradient(#b7f54c, #049674);
+   font-weight: 700;
+   color: #141414;
+   font-size:14px;
+   transition: all .2s;
+}
+button:hover {
+   transition: .2s;
+   background: linear-gradient(#b9e66d, #049674);
+}
+</style>
