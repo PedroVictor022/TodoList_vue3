252d7f5e58232c0b40bb47c144c3044cddb64144
Pedro Victor <pedro20002victor@gmail.com>
2022-10-24 22:37:09 -0300

    Mark todo is complete

--- src/App.vue
+++ src/App.vue
@@ -10,22 +10,3 @@ export default {
   }
 }
 </script>
-
-
-<style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: sans-serif;
-}
-
-body {
-  background: #222;
-  font-family: sans-serif;
-  padding-bottom: 1em;
-  width: 80%;
-  margin: 0 auto;
-
-}
-</style>
--- src/component/TodoApp.vue
+++ src/component/TodoApp.vue
@@ -6,9 +6,12 @@
          <input type="text" name="newTodo" v-model="newTodo">
          <button>Add New Todo</button>
 
-         <div class="todos">
-            <p>{{ newTodo }}</p>
-         </div>
+         <ul class="todos" v-for="todo in todos" :key="todo.id">
+            <li 
+               @click="toggleDone(todo)"
+               :class="{ done: todo.done }"
+            >{{ todo.content }}</li>
+         </ul>
       </form>
 
    </div>
@@ -23,13 +26,26 @@ export default {
       const newTodo = ref("");
       const todos = ref([]);
 
+      function generateId() {
+         return Math.floor(Math.random() * 1000).toFixed(2)
+      }
+
       function addNewTodo(e) {
          e.preventDefault();
-         console.log(newTodo.value);
+
          todos.value.push({
+            id: generateId(),
             done: false,
             content: newTodo.value
          });
+
+         console.log(newTodo.value);
+         newTodo.value = "";
+      }
+
+      function toggleDone(todo) {
+         todo.done = !todo.done
+         console.log(todo.done ? "Complete" : "No Complete")
       }
 
       // Ao usar setup() temos que 'expor os dados retornados por ele, usando o return'
@@ -37,72 +53,33 @@ export default {
          todos,
          newTodo,
          addNewTodo,
+         toggleDone,
       }
    }
 }
 </script>
 
 <style>
-.container {
-   margin: 0 auto;
-   width: 420px;
-
-   display: flex;
-   flex-direction: column;
-   align-items: center;
-   justify-content: center;
-}
-
-.title-app {
-   font-size: 36px;
-   background: -webkit-linear-gradient(#b7f54c, #049674);
-   -webkit-background-clip: text;
-   -webkit-text-fill-color: transparent;
-
-   margin-top: 2rem;
-   margin-bottom: 1rem;
-}
-
-form {
-   display: flex;
-   flex-direction: column;
-   gap: 0.8rem;
-
-   padding: 1rem;
-   border-radius: 6px;
-
-   background: #141414;
-
-   width: 320px;
-}
-
-label {
-   font-size: 22px;
+body {
+   background: #222;
    color: #fefefe;
+  font-family: sans-serif;
+  padding-top: 1em;
+  padding-bottom: 1em;
+  width: 60%;
+  margin: 0 auto;
 }
-
-input {
-   width: 100%;
-   padding: 0.2rem;
-   border: none;
-   outline: none;
-   border-radius: 4px;
+input, textarea, button, p, div, section, article, select {
+  display: 'block';
+  width: 100%;
+  font-family: sans-serif;
+  font-size: 1em;
+  margin: 0.5em;
 }
-
-button {
-   cursor: pointer;
-   width: 100%;
-   padding: 0.2rem;
-   border-radius: 4px;
-   background: linear-gradient(#b7f54c, #049674);
-   font-weight: 700;
-   color: #141414;
-   font-size: 14px;
-   transition: all .2s;
+.todo {
+  cursor: pointer;
 }
-
-button:hover {
-   transition: .2s;
-   background: linear-gradient(#b9e66d, #049674);
+.done {
+  text-decoration: line-through;
 }
 </style>
